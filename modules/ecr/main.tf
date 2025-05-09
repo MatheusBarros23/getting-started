6d67b5b1dbe3ef985a0f490731afd955251b4e41
Matheus Barros <88664949+MatheusBarros23@users.noreply.github.com>
2025-05-09 12:50:18 -0300

    Update main.tf

--- modules/ecr/main.tf
+++ modules/ecr/main.tf
@@ -1,14 +1,22 @@
+# Verifica se o repositório já existe
+data "aws_ecr_repository" "existing" {
+  count = var.create_if_not_exists ? 1 : 0
+  name  = var.repository_name
+  
+  # Este bloco ignora o erro caso o repositório não exista
+  depends_on = [
+    # Sem dependências
+  ]
+}
+
+locals {
+  # Determina se é necessário criar o repositório com base no resultado da data source
+  repository_exists = try(length(data.aws_ecr_repository.existing) > 0, false)
+}
+
 resource "aws_ecr_repository" "app_repository" {
   name                 = var.repository_name
   image_tag_mutability = var.image_tag_mutability
-
-  lifecycle {
-    prevent_destroy = true
-    ignore_changes = [
-      image_scanning_configuration,
-      encryption_configuration
-    ]
-  }
 
   image_scanning_configuration {
     scan_on_push = var.scan_on_push
@@ -19,6 +27,12 @@ resource "aws_ecr_repository" "app_repository" {
   }
 
   tags = var.tags
+}
+
+# Usa um recurso que já existe ou o recém-criado
+locals {
+  repository_name = local.repository_exists ? data.aws_ecr_repository.existing[0].name : aws_ecr_repository.app_repository[0].name
+  repository_url  = local.repository_exists ? data.aws_ecr_repository.existing[0].repository_url : aws_ecr_repository.app_repository[0].repository_url
 }
 
 resource "aws_ecr_lifecycle_policy" "app_lifecycle_policy" {
